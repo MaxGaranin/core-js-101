658224ab7fe134b02e410e7aa3df05e1225f8b8d
GaraninMS <GaraninMS@samnipineft.ru>
2019-12-26 10:57:22 +0400

    feat: solve matrix multiply

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -461,17 +461,26 @@ function getCommonDirectoryPath(pathes) {
  *                         [ 6 ]]
  *
  */
-function getMatrixProduct(/* m1, m2 */) {
-  throw new Error('Not implemented');
-  // const matrix = [];
+function getMatrixProduct(m1, m2) {
+  const result = [];
 
-  // for (let i = 0; i < m1.length; i += 1) {
-  //   matrix[i] = [];
-  //   let result = 0;
-  //   for (let j = 0; j < m2.length; j += 1) {
-  //     result += m1[i][j] * m2[j][i];
-  //   }
-  // }
+  for (let i = 0; i < m1.length; i += 1) {
+    if (m1[i].length !== m2.length) {
+      throw new Error('m1 rows size must be equals to m2 columns size');
+    }
+
+    result[i] = [];
+
+    for (let j = 0; j < m2[i].length; j += 1) {
+      result[i][j] = 0;
+
+      for (let k = 0; k < m2.length; k += 1) {
+        result[i][j] += m1[i][k] * m2[k][j];
+      }
+    }
+  }
+
+  return result;
 }
 
 /**
